97572b6e250e8df10daf0ad346b173318453f07a
Vinay Pulim <v@pulim.com>
2018-12-27 22:05:44 -0500

    wip

--- test/sync/fetcher/blockfetcher.js
+++ test/sync/fetcher/blockfetcher.js
@@ -1,7 +1,6 @@
 const tape = require('tape-catch')
 const td = require('testdouble')
 const BN = require('bn.js')
-const Block = require('ethereumjs-block')
 const EventEmitter = require('events')
 const { defaultLogger } = require('../../../lib/logging')
 defaultLogger.silent = true
@@ -16,71 +15,42 @@ tape('[BlockFetcher]', t => {
   PeerPool.prototype.ban = td.func()
   td.replace('../../../lib/net/peerpool', PeerPool)
   const BlockFetcher = require('../../../lib/sync/fetcher/blockfetcher')
-  const ONE = new BN(1)
-  const TWO = new BN(2)
 
   t.test('should start/stop', async (t) => {
     const fetcher = new BlockFetcher({
       pool: new PeerPool(),
       first: new BN(1),
       count: 10,
+      maxPerRequest: 5,
       timeout: 5
     })
+    fetcher.next = () => false
     t.notOk(fetcher.running, 'not started')
-    fetcher.start()
+    fetcher.fetch()
+    t.equals(fetcher.in.size(), 2, 'added 2 tasks')
     await wait()
     t.ok(fetcher.running, 'started')
-    fetcher.stop()
+    fetcher.destroy()
     await wait()
     t.notOk(fetcher.running, 'stopped')
     t.end()
   })
 
-  t.test('should fetch', async (t) => {
-    const pool = new PeerPool()
-    const fetcher = new BlockFetcher({
-      pool,
-      first: new BN(1),
-      count: 2
-    })
-    const peer = {eth: td.object()}
-    const headers = [new Block.Header({number: 1}), new Block.Header({number: 12})]
-    const bodies = [[[], []], [[], []]]
-    const blocks = [new Block([headers[0]].concat(bodies[0])), new Block([headers[1]].concat(bodies[1]))]
-    td.when(pool.idle(td.matchers.anything())).thenReturn(peer)
-    td.when(peer.eth.getBlockHeaders({block: ONE, max: 2})).thenResolve(headers)
-    td.when(peer.eth.getBlockBodies(td.matchers.anything())).thenResolve(bodies)
-    fetcher.start()
-    fetcher.on('data', (data) => {
-      t.deepEquals(
-        data.map(r => [].concat(...r.raw)),
-        blocks.map(b => [].concat(...b.raw)),
-        'got blocks'
-      )
-      t.end()
-    })
+  t.test('should process', t => {
+    const fetcher = new BlockFetcher({pool: new PeerPool()})
+    const blocks = [{header: {number: 1}}, {header: {number: 2}}]
+    t.deepEquals(fetcher.process({task: {count: 2}}, {blocks}), blocks, 'got results')
+    t.notOk(fetcher.process({task: {count: 2}}, {blocks: []}), 'bad results')
+    t.end()
   })
 
-  // t.test('should process', t => {
-  //   const fetcher = new BlockFetcher({pool: new PeerPool()})
-  //   const blocks = [{header: {number: 1}}, {header: {number: 2}}]
-  //   fetcher.running = true
-  //   fetcher.add = td.func()
-  //   fetcher.process({task: 'task'}, {blocks: []})
-  //   td.verify(fetcher.add('task'))
-  //   fetcher.process({task: {last: TWO}}, {blocks})
-  //   setTimeout(() => {
-  //     td.verify(fetcher.add({first: ONE, last: TWO}))
-  //     t.pass('processed tasks')
-  //     t.end()
-  //   }, 10)
-  // })
-  //
-  // t.test('should check if peer fetchable', async (t) => {
-  //   const fetcher = new BlockFetcher({pool: new PeerPool()})
-  //   t.ok(fetcher.fetchable({eth: {}}), 'fetchable')
-  //   t.end()
-  // })
+  t.test('should find a fetchable peer', async (t) => {
+    const pool = new PeerPool()
+    const fetcher = new BlockFetcher({pool})
+    td.when(fetcher.pool.idle(td.matchers.anything())).thenReturn('peer0')
+    t.equals(fetcher.peer(), 'peer0', 'found peer')
+    t.end()
+  })
 
   t.test('should reset td', t => {
     td.reset()
--- test/sync/fetcher/headerfetcher.js
+++ test/sync/fetcher/headerfetcher.js
@@ -1,74 +1,31 @@
 const tape = require('tape-catch')
 const td = require('testdouble')
-const BN = require('bn.js')
+// const BN = require('bn.js')
 const EventEmitter = require('events')
 const { defaultLogger } = require('../../../lib/logging')
 defaultLogger.silent = true
 
 tape('[HeaderFetcher]', t => {
   class PeerPool extends EventEmitter {}
+  PeerPool.prototype.idle = td.func()
+  PeerPool.prototype.ban = td.func()
   td.replace('../../../lib/net/peerpool', PeerPool)
-  const HeaderQueue = td.constructor()
-  HeaderQueue.prototype.open = td.func()
-  HeaderQueue.prototype.add = td.func()
-  td.when(HeaderQueue.prototype.open()).thenResolve()
-  const headers = [{number: 1}, {number: 2}]
-  td.when(HeaderQueue.prototype.add(headers)).thenReject(new Error('err0'))
-  td.replace('../../../lib/sync/queue', { HeaderQueue })
   const HeaderFetcher = require('../../../lib/sync/fetcher/headerfetcher')
-  const ONE = new BN(1)
-  const TWO = new BN(2)
 
-  t.test('should order correctly', t => {
-    const fetcher = new HeaderFetcher({pool: new PeerPool()})
-    t.ok(fetcher.before({first: ONE}, {first: TWO}), 'ordered')
-    t.notOk(fetcher.before({first: TWO}, {first: ONE}), 'not ordered')
-    t.end()
-  })
-
-  t.test('should open', async (t) => {
-    const fetcher = new HeaderFetcher({pool: new PeerPool()})
-    t.notOk(fetcher.opened, 'not open')
-    await fetcher.open()
-    t.ok(fetcher.opened, 'open')
-    t.end()
-  })
-
-  t.test('should fetch', async (t) => {
-    const fetcher = new HeaderFetcher({
-      interval: 1,
-      pool: new PeerPool(),
-      flow: td.object(),
-      maxPerRequest: 128
-    })
-    const peer = {les: td.object()}
-    td.when(fetcher.flow.maxRequestCount(peer, 'GetBlockHeaders')).thenReturn(0)
-    t.equals(await fetcher.fetch({}, peer), false, 'reached request limit')
-    td.when(fetcher.flow.maxRequestCount(peer, 'GetBlockHeaders')).thenReturn(1000)
-    td.when(peer.les.getBlockHeaders({block: ONE, max: 2})).thenResolve([])
-    td.when(peer.les.getBlockHeaders({block: ONE, max: 128})).thenResolve([])
-    t.deepEquals(await fetcher.fetch({first: ONE, last: TWO}, peer), [], 'got headers')
-    t.deepEquals(await fetcher.fetch({first: ONE, last: new BN(1000)}, peer), [], 'got max headers')
-    t.end()
-  })
-
-  t.test('should process', async (t) => {
+  t.test('should process', t => {
     const fetcher = new HeaderFetcher({pool: new PeerPool(), flow: td.object()})
-    fetcher.running = true
-    fetcher.add = td.func()
-    fetcher.process({task: 'task'}, {headers: []})
-    td.verify(fetcher.add('task'))
-    fetcher.process({task: {last: TWO}}, {headers})
-    setTimeout(() => {
-      td.verify(fetcher.add({first: ONE, last: TWO}))
-      t.pass('processed tasks')
-      t.end()
-    }, 10)
+    const headers = [{number: 1}, {number: 2}]
+    t.deepEquals(fetcher.process({task: {count: 2}, peer: 'peer0'}, {headers, bv: 1}), headers, 'got results')
+    t.notOk(fetcher.process({task: {count: 2}}, {headers: []}), 'bad results')
+    td.verify(fetcher.flow.handleReply('peer0', 1))
+    t.end()
   })
 
-  t.test('should check if peer fetchable', async (t) => {
-    const fetcher = new HeaderFetcher({pool: new PeerPool(), sync: td.object()})
-    t.ok(fetcher.fetchable({les: {status: {serveHeaders: 1}}}), 'fetchable')
+  t.test('should find a fetchable peer', async (t) => {
+    const pool = new PeerPool()
+    const fetcher = new HeaderFetcher({pool})
+    td.when(fetcher.pool.idle(td.matchers.anything())).thenReturn('peer0')
+    t.equals(fetcher.peer(), 'peer0', 'found peer')
     t.end()
   })
 
